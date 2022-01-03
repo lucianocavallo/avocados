42e2b1aa92075def6e6fce0c1f0a6ec35a2055c3
luchx88 <lucianocavallo@gmail.com>
2022-01-03 18:04:04 -0300

    Rutas Basicas

---
+++ pages/about.js
@@ -0,0 +1,13 @@
+// Componente de React que trabaje como una pagina
+
+import React from "react";
+
+const About = () => {
+  return (
+    <div>
+      <h1>Esta es la pagina de about</h1>
+    </div>
+  );
+}
+
+export default About;
---
+++ pages/index.js
@@ -0,0 +1,13 @@
+// Componente de React que trabaje como una pagina
+
+import React from "react";
+
+const Home = () => {
+  return (
+    <div>
+      <h1>Hola Platzi!</h1>
+    </div>
+  );
+}
+
+export default Home;
