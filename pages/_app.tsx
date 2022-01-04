1ccee9f9c5ab754e233de41918beb06f9a9de8c9
luchx88 <lucianocavallo@gmail.com>
2022-01-04 03:30:57 -0300

    Module CSS

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,6 @@
 import { AppProps } from 'next/app';
 import Layout from '@components/Layout/Layout';
 
-import "../style.css";
-
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <Layout>
