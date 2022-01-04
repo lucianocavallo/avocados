cc8c7fe811b7d7bedcae6890367f10d1c615993a
luchx88 <lucianocavallo@gmail.com>
2022-01-04 02:08:50 -0300

    Creando y consumiendo nuestra propia API

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,24 @@
-import React from "react";
-import Navbar from "../components/Navbar/Navbar";
+import React, { useEffect, useState } from "react";
+import Navbar from "@components/Navbar/Navbar";
 
 const Home = () => {
+  const [productList, setProductList] = useState<TProduct[]>([]);
+
+  useEffect(() => {
+    window.fetch('/api/avo')
+    .then(response => response.json())
+    .then(({ data, length }) => {
+      setProductList(data);
+    })
+  }, [])
+
   return (
     <div>
       <Navbar />
       <h1>Hola Platzi!</h1>
+      {productList.map(product => (
+        <div key={product.id}>{product.name}</div>
+      ))}
     </div>
   );
 }
--- pages/product/[productId].tsx
+++ pages/product/[productId].tsx
@@ -1,16 +1,42 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { useRouter } from "next/router";
 
-const ProductItem = () => {
-  // const { query: { productId } } = useRouter();
-  const router = useRouter();
+// const InitialProduct: TProduct = {
+//   id: '',
+//   name: '',
+//   sku: '',
+//   price: 0,
+//   image: '',
+//   attributes: {
+//     description: '',
+//     shape: '',
+//     hardiness: '',
+//     taste: ','
+//   }
+// }
 
-  // console.log(productId);
-  console.log(router.query);
+const ProductItem = () => {
+  const { query: { productId }} = useRouter();
+  const [product, setProduct] = useState({});
+
+  useEffect(() => {
+    if(productId) {
+      window.fetch(`/api/avo/${productId}`)
+      .then(res => res.json())
+      .then(product => setProduct(product))
+      .catch(error => console.error(error));
+    }
+  }, [productId])
 
   return (
     <div>
-      <h1>Esta es la pagina del producto: {router.query.productId}</h1>
+      {Object.keys(product).length &&(
+        <>
+          <h1>Esta es la pagina del producto: {product?.name}</h1>
+          <p>Precio: {product?.price}</p>
+          <p>Descripcion: {product?.attributes.description}</p>
+        </>
+      )}
     </div>
   );
 }
